b1468aa0b3426ae0493d57e456d518b6c7c55029
Bayulu Yakubu <105476695+Yakubuh@users.noreply.github.com>
2023-03-14 01:59:31 +0000

    Update 1-multi_languages.js

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,4 +1,5 @@
 #!/usr/bin/node
-console.log("c is fun");
-console.log("Python is cool");
-console.log("JavaScript is amazing");
+
+console.log('c is fun');
+console.log('Python is cool');
+console.log('avaScript is amazing')
